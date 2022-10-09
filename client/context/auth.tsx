5e3572ce92c207c4630822f9dcd26e9871a9775d
Logan Thomas <logan.thomas@forsta.com>
2022-10-09 17:50:09 -0600

    removed client/ from gitignore

---
+++ client/context/auth.tsx
@@ -0,0 +1,33 @@
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useState,
+} from "react";
+
+export interface IUser {
+  username: string;
+  email: string;
+  url: string;
+}
+
+interface IAuthContext {
+  user: IUser | null;
+  setUser: Dispatch<SetStateAction<IUser | null>>;
+}
+
+const AuthContext = createContext<any>({});
+
+export const AuthProvider = ({ children }: any) => {
+  const [user, setUser] = useState(null);
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
