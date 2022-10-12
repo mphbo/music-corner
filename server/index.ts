c279cb923c784fdeb5639e6cb4ebe4b040926d3a
Logan Thomas <logan.thomas@forsta.com>
2022-10-12 02:04:10 -0600

    deploying temporary change to fix production database

--- server/index.ts
+++ server/index.ts
@@ -32,6 +32,12 @@ pgClient.on("connect", (client) => {
 app.get("/", (req, res) => {
   res.send("Hi");
 });
+app.delete("/users/", async (req, res) => {
+    // Delete user
+    const response = await pgClient.query(`DELETE FROM users WHERE email=;`);
+    res.send(response.rows);
+  });
+)
 
 import usersRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
