0197296917271c404620c27713ac0aba74f9b76f
Logan Thomas <logan.thomas@forsta.com>
2022-10-25 20:40:48 -0600

    removed features in nextjs config

--- client/next.config.js
+++ client/next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    unoptimized: true,
-  },
-  experimental: {
-    outputStandalone: true,
-  },
 };
 
 module.exports = nextConfig;
