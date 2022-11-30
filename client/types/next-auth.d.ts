fada12d3496a09c87da8f7260108b2afcfaf569f
Logan Thomas <logan.thomas@forsta.com>
2022-11-30 01:23:11 -0700

    updated session type

--- client/types/next-auth.d.ts
+++ client/types/next-auth.d.ts
@@ -5,12 +5,10 @@ declare module "next-auth" {
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
-    user: {
-      /** The user's postal address. */
-      id: number;
-      email: string;
-      url: string;
-      username: string;
-    } & DefaultSession["user"];
+    /** The user's postal address. */
+    id: number;
+    email: string;
+    url: string;
+    username: string;
   }
 }
